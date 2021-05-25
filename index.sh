55307b2b7f0be886d72b385bdbbecb0633093d9f
Paul Aglipay <paglipay@gmail.com>
2021-05-25 16:06:10 -0700

    test

--- index.sh
+++ index.sh
@@ -1,2 +1,2 @@
 echo 'Hello'
-cd .. && git clone https://github.com/paglipay/DTree.git
+cd .. && git clone https://github.com/paglipay/DTree.git && git clone https://github.com/paglipay/React-Portfolio.git
