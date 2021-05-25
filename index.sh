a0dbb6109a2eb450b598d7cb759185981febbd7a
Paul Aglipay <paglipay@gmail.com>
2021-05-25 15:41:17 -0700

    test

--- index.sh
+++ index.sh
@@ -1,11 +1,11 @@
-echo 'Hello'
-sudo apt-get install expect
+# echo 'Hello'
+# sudo apt-get install expect
 # git clone https://github.com/paglipay/DTree.git
 #!/usr/bin/expect -f
 
 set timeout -1
 
-spawn ./questions
+spawn ./questions.sh
 
 expect "Hello, who are you?\r"
 
