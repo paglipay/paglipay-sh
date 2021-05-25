9f59ebdb8f0d8b8c0c62004f7814cabe2a94335c
Paul Aglipay <paglipay@gmail.com>
2021-05-25 15:57:32 -0700

    test

--- index.sh
+++ index.sh
@@ -1,19 +1,2 @@
-#!/usr/bin/expect -f
-
-set timeout -1
-
-spawn ./questions.sh
-
-expect "Hello, who are you?\r"
-
-send -- "Im Adam\r"
-
-expect "Can I ask you some questions?\r"
-
-send -- "Sure\r"
-
-expect "What is your favorite topic?\r"
-
-send -- "Technology\r"
-
-expect eof
+echo 'Hello'
+cd .. && git clone https://github.com/paglipay/DTree.git
